e08a36ec6cae309847e2fdb7aec73e61665c7a58
Dave <dbraun31@gmail.com>
2025-10-13 11:13:46 -0400

    Revise: remove large data

--- analysis/scripts/sandbox/run_ml/quick_viz.r
+++ analysis/scripts/sandbox/run_ml/quick_viz.r
@@ -1,28 +1,123 @@
 rm(list=ls())
 library(tidyverse)
+library(ggpubr)
+library(RColorBrewer)
+library(scales)
 library(reticulate)
+library(fs)
 use_condaenv('eeg-fmri')
+root <- path('analysis/scripts/sandbox/run_ml')
 
 py_run_string("
 import numpy as np
-truth = np.load('truth.npy')
-surrogates = np.load('surrogates.npy')
-              ")
+import pickle
+import os
+import mne
+from pyprojroot import here
+from pathlib import Path
+os.chdir(here())
+root = Path('analysis/scripts/sandbox/run_ml')
+
+with open(root / Path('shap_networks.pkl'), 'rb') as file:
+    shap_networks = pickle.load(file)
+
+with open(root / Path('truth.pkl'), 'rb') as file:
+    truth = pickle.load(file)
+
+with open(root / Path('surrogates.pkl'), 'rb') as file:
+    surrogates = pickle.load(file)
+
+
+fname = Path('sub-001_ses-001_bld001_eeg_Bergen_CWreg_filt_ICA_rej.set')
+raw = mne.io.read_raw_eeglab(Path('analysis/data/original/sub-001/ses-001/eeg') / fname)
+ch_names = raw.info['ch_names']
+")
 
 truth <- py$truth
+truth <- sapply(truth, mean)
+shaps <- lapply(py$shap_networks, FUN = function(x) colMeans(abs(x)))
 surrogates <- py$surrogates
+ch_names <- py$ch_names
+bands <- c('Delta', 'Theta', 'Alpha', 'Beta', 'Gamma')
 
-data.frame(surrogates=surrogates) %>% 
-    ggplot(aes(x = surrogates)) + 
+# Compile shaps
+cols_df <- expand.grid(channel = ch_names, band = bands, lag = 1:6)
+cols <- with(cols_df, paste(channel, band, lag, sep='_'))
+
+shap_df <- bind_rows(
+    lapply(names(shaps), function(nm) {
+        d <- data.frame(col=cols, network=nm, shap = shaps[[nm]])
+        d
+    })
+)
+
+
+shap_df <- shap_df %>% 
+    separate(col, into = c('channel', 'band', 'lag'), sep = '_') %>% 
+    mutate(band = factor(band, levels = bands)) %>% 
+    group_by(band, lag, network) %>% 
+    summarize(shap = mean(shap)) 
+
+
+text <- 20
+# Format surrogates
+truth_df <- data.frame(network = names(truth), m = truth)
+surrogates_df <- data.frame(do.call(cbind, lapply(surrogates, FUN = function(x) unlist(x))))
+p1 <- surrogates_df %>% 
+    mutate(iteration = 1:n()) %>% 
+    gather(network, surrogate, -iteration) %>% 
+    inner_join(truth_df) %>% 
+    ggplot(aes(x = surrogate)) + 
     geom_histogram(color = 'black', fill = 'steelblue', bins = 20) +
-    geom_point(aes(x = mean(!!truth), y = 1), shape = 8, color = 'steelblue', size = 4) + 
+    geom_point(aes(x = m, y = 50), shape = 8, color = 'steelblue', size = 4) + 
+    facet_wrap(~network, nrow=1) + 
     labs(
         x = latex2exp::TeX('$R^2$'),
-        y = 'Frequency'
+        y = 'Frequency',
+        caption = 'p = .002'
     ) + 
     theme_bw() + 
     theme(panel.grid = element_blank(),
           axis.ticks = element_blank(),
-          text = element_text(size = 16))
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          text = element_text(size = text),
+          strip.background = element_rect(fill = 'NA'))
 
-ggsave('surrogate_result.png', height = 6, width = 6, units = 'in', dpi = 300)
+ggsave(filename=path(root, 'surrogate_result.png'), plot = p1, height = 6, width = 10, units = 'in', dpi = 300)
+
+
+# Plot shaps
+min_shap <- min(shap_df$shap)
+max_shap <- max(shap_df$shap)
+
+p2 <- shap_df %>% 
+    mutate(lag = factor((as.numeric(lag) * 2) - 2)) %>% 
+    ggplot(aes(x = band, lag)) + 
+    geom_tile(aes(fill = shap)) +
+    scale_fill_gradientn(colors = brewer.pal(11, 'Reds')) +   
+                         #values = rescale(c(min(shap), 0, max(shap)))) +
+    labs(
+        x = 'EEG Frequency Band',
+        y = 'Lag (s)',
+        fill = 'SHAP Feature Importance',
+        title = 'Mean absolute value importance'
+    ) + 
+    facet_wrap(~network, nrow=1) + 
+    theme_bw() + 
+    theme(axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          legend.position = 'bottom',
+          text = element_text(size = text),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.text = element_text(angle = 45, hjust = 1, size = 14),
+          strip.background = element_rect(fill = NA, color = 'black'))
+    
+                         
+ggsave(path(root, 'SHAP_abs.png'), height = 6, width = 12, units = 'in', dpi = 300)
+
+
+g <- ggarrange(p1, p2, nrow = 2, labels = c('A.', 'B.'))
+g
+
+ggsave(filename = path(root, 'surrogate_shap.png'), plot = g, height = 8, 
+       width = 14, units = 'in', dpi = 300)
--- analysis/scripts/sandbox/run_ml/xgb.py
+++ analysis/scripts/sandbox/run_ml/xgb.py
@@ -5,12 +5,15 @@ from tqdm import tqdm
 import sys
 modules_root = here() / Path('analysis/scripts/modules')
 os.chdir(here() / Path('analysis'))
+sys.path.append(str(here()))
 sys.path.append(str(modules_root))
 from modeling.model_selection import train_test_across
 from modeling.dummy import BaselineModel
 from utility import get_subjects
 root = Path(modules_root / Path('../sandbox/run_ml'))
 import numpy as np
+import shap
+from concurrent.futures import ThreadPoolExecutor
 import pandas as pd
 from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -23,33 +26,26 @@ from glob import glob
 def rmse(y_true, y_pred):
     return np.sqrt(np.mean((y_true - y_pred)**2))
 
-dpath = Path('data/formatted/reduced')
-subjects = get_subjects(dpath)
-
 def get_p(surrogates, true_loss):
     return (np.sum(surrogates >= true_loss) + 1) / (len(surrogates) + 1)
 
-def get_loss(subjects, surrogate=False, nsims=500):
+def get_loss(subjects, network, surrogate):
 	
-    if not surrogate:
-        nsims=1
-	
-    out = []
+    losses = []
 
-    for sim in range(1, nsims+1):
+    def prep_subject(subject, network, surrogate):
+        X_train, y_train, X_test, y_test = train_test_across(network=network, held_out_subject=subject)
+        
+        if surrogate:
+            np.random.shuffle(y_train)
+        return X_train, y_train, X_test, y_test
 
-        print(f'\n\n--- SIMULATION {sim} OF {nsims}---\n\n')
 
-        loss = []
-
-        for subject in tqdm(subjects, desc='Processing subjects'):
-
-            # Import data
-            X_train, y_train, X_test, y_test = train_test_across(subject)
-
-            if surrogate:
-                np.random.shuffle(y_train)
-
+    # CPU parallelize the data prep
+    with ThreadPoolExecutor(max_workers=16) as pool:
+        futures = [pool.submit(prep_subject, subject, network, surrogate) for subject in subjects]
+        for fut in futures:
+            X_train, y_train, X_test, y_test = fut.result()
             # XGBoost
             xgb = XGBRegressor(n_estimators=300,
                                max_depth=3,
@@ -57,35 +53,112 @@ def get_loss(subjects, surrogate=False, nsims=500):
                                subsample=.8,
                                colsample_bytree=.8,
                                tree_method='gpu_hist',
-                               preditor='gpu_predictor',
+                               predictor='gpu_predictor',
                                verbosity=0)
-
-
             xgb.fit(X_train, y_train)
             y_pred = xgb.predict(X_test)
-            loss.append(r2_score(y_test, y_pred))
+            losses.append(r2_score(y_test, y_pred))
 
-        out.append(np.array(loss).mean())
-
-    if not surrogate:
-        return np.array(loss)
-
-    return np.array(out)
+    return np.array(losses), xgb
 
 
+def get_shap(model, network):
+    X, y = train_test_across(network=network)
+    model.fit(X, y)
 
-surrogates = get_loss(subjects, surrogate=True, nsims=50)
+    explainer = shap.TreeExplainer(model)
 
-# Combine with existing if they exist
-if os.path.exists(root / Path('surrogates.npy')):
-    surrogates_old = np.load(root / Path('surrogates.npy'))
-    surrogates = np.concatenate([surrogates, surrogates_old], axis=0)
+    shap_values = explainer.shap_values(X)
 
-# Write surrogates
-np.save(root / Path('surrogates.npy'), surrogates)
+    return shap_values
+    
 
-# Get and write truth
-truth = get_loss(subjects)
-p = get_p(surrogates, truth.mean())
-np.save(root / Path('truth.npy'), truth)
+def get_surrogates(nsims, networks, subjects, 
+            out_path=Path('scripts/sandbox/run_ml/surrogates.pkl')):
+
+    if not os.path.exists(out_path):
+        out = {x: [] for x in networks}
+    else:
+        with open(out_path, 'rb') as file:
+            out = pickle.load(file)
+
+    start = len(out[networks[0]]) + 1
+
+    for sim in range(start, nsims+1):
+        print(f'\n---SIMULATION {sim} of {nsims}---\n')
+
+        for network in networks:
+            print(f'Computing network {network}')
+
+            loss, _ = get_loss(subjects, network, surrogate=True)
+
+            out[network].append(loss.mean())
+
+        # Write after each sim
+        with open(out_path, 'wb') as file:
+            pickle.dump(out, file)
+
+
+
+
+# --- GET SUBJECTS AND NETWORKS --- #
+dpath = Path('data/formatted/reduced')
+subjects = get_subjects(dpath)
+
+dpath = Path('data/formatted/reduced/sub-001.pkl')
+
+with open(dpath, 'rb') as file:
+    d = pickle.load(file)
+
+networks = d['ses-001']['run-001']['y'].keys()
+networks = [x for x in networks if x not in ['DAN', 'DMN']]
+
+
+
+# --- GET AND WRITE SURROGATES --- #
+
+
+get_surrogates(500, networks, subjects)
+
+
+
+
+# --- GET AND WRITE TRUTH --- #
+out = {}
+for network in networks:
+    # XGBoost
+    xgb = XGBRegressor(n_estimators=300,
+                       max_depth=3,
+                       learning_rate=.05,
+                       subsample=.8,
+                       colsample_bytree=.8,
+                       tree_method='gpu_hist',
+                       predictor='gpu_predictor',
+                       verbosity=0)
+
+    out[network] = get_shap(xgb, network)
+    
+
+with open(root / Path('shap_networks.pkl'), 'wb') as file:
+    pickle.dump(out, file)
+
+
+# Get truth
+truth = {}
+for network in networks:
+    print(f'Processing network {network}')
+    truth[network], _ = get_loss(subjects, network, surrogate=False)
+
+with open(root / Path('truth.pkl'), 'wb') as file:
+    pickle.dump(truth, file)
+
+
+out = {'truth': truth, 'shap': get_shap(xgb)}
+with open(root / Path('truth.pkl'), 'wb') as file:
+    pickle.dump(out, file)
+
+
+
+
+
 
