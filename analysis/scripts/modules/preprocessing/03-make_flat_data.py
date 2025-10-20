990b63235126b9336c03d19ded51dcb400be7849
Dave Braun <dbraun31@gmail.com>
2025-10-20 12:16:15 -0400

    Restructure the correlation processing scripts to preprocessing

---
+++ analysis/scripts/modules/preprocessing/03-make_flat_data.py
@@ -0,0 +1,85 @@
+import mne
+import numpy as np
+import pandas as pd
+from pyprojroot import here
+import os
+from glob import glob
+from pathlib import Path
+import pickle
+from itertools import product
+
+'''
+This script converts the nested dict data in formatted/full to a flat data
+frame that can be used to compute correlations. Output is stored in 
+'''
+
+
+
+def process_subject(subject, col_names, data_path=Path('data/formatted')):
+
+    file = data_path / Path(f'{subject}.pkl')
+    with open(file, 'rb') as file:
+        data = pickle.load(file)
+
+    subject_d = pd.DataFrame()
+
+    for session in data:
+        for run in data[session]:
+
+            run_data = data[session][run]
+            run_df = pd.DataFrame(run_data['X'], columns=col_names)
+            run_df.insert(0, 'subject', subject)
+            run_df.insert(1, 'session', session)
+            run_df.insert(2, 'run', run)
+            run_df.insert(3, 'tr', list(range(1, run_df.shape[0]+1)))
+            
+            networks = list(data[session][run]['y'].keys())
+            for idx, network in enumerate(networks, start=4):
+                run_df.insert(idx, network, data[session][run]['y'][network])
+
+            subject_d = pd.concat([subject_d, run_df], axis=0)
+
+    return subject_d
+
+
+def make_column_names(channels):
+    freqs = list(range(1, 41))
+    lags = list(range(11))
+    
+    col_names_tuple = list(product(channels, freqs, lags))
+
+    col_names = []
+    for name_tuple in col_names_tuple:
+        name = '_'.join([str(x) for x in name_tuple])
+        col_names.append(name)
+
+    return col_names
+
+
+if __name__ == '__main__':
+
+
+    # Parse input args
+    args = sys.argv[1:]
+
+    if not args:
+        data_root = Path('analysis/data')
+        os.chdir(here())
+        sys.path.append(str(here()))
+    else:
+        data_root = Path(args[0])
+
+    raw = mne.io.read_raw_eeglab(f'{data_root}/original/sub-001/ses-001/eeg/sub-001_ses-001_bld001_eeg_Bergen_CWreg_filt_ICA_rej.set')
+    channels = raw.info['ch_names']
+    col_names = make_column_names(channels)
+
+    subjects = [Path(x).stem for x in glob(f'{data_root}/formatted/*')]
+
+    d = pd.DataFrame()
+
+    # Process each subject and concatenate
+    for subject in subjects:
+        new_d = process_subject(subject, col_names, data_path=f'{data_root}/formatted')
+        d = pd.concat([d, new_d], axis=0)
+
+    d.reset_index(drop=True).to_feather(f'{data_root}/merged_data.feather')
---
+++ analysis/scripts/sandbox/correlations/utils/02-make_long_data.r
@@ -0,0 +1,107 @@
+rm(list=ls())
+library(tidyverse)
+library(data.table)
+library(future.apply)
+library(here)
+library(fs)
+library(arrow)
+setwd(here())
+root <- path('analysis/scripts/sandbox/correlations')
+
+
+# Function defs
+
+extract_matrices <- function(run_df, eeg_cols, fmri_cols) {
+    eeg_mat <- as.matrix(run_df[, eeg_cols])
+    fmri_mat <- as.matrix(run_df[, fmri_cols])
+    return(list(eeg=eeg_mat, fmri=fmri_mat))
+}
+
+compute_spearman_cor <- function(eeg, fmri) {
+    # Apply column-wise rank
+    eeg_rank <- apply(eeg, 2, rank)
+    fmri_rank <- apply(fmri, 2, rank)
+    
+    # Normalize
+    eeg_z <- scale(eeg_rank)
+    fmri_z <- scale(fmri_rank)
+    n_trs <- nrow(eeg)
+    
+    # Correlate
+    cor <- t(fmri_z) %*% eeg_z / (n_trs - 1)
+    
+    return(cor)
+    
+}
+
+
+# Run 
+
+if(file.exists(path(root, 'cors.rds'))){
+    cors <- readRDS(path(root, 'cors.rds'))
+} else {
+    
+    # Import
+    d <- read_feather('analysis/data/merged_data.feather')
+    
+    # Extract modality columns
+    eeg_cols <- colnames(d)[which(colnames(d) == 'Fp1_1_0'):(ncol(d))]
+    fmri_cols <- colnames(d)[(which(colnames(d) == 'tr')+1):(which(colnames(d) == 'Fp1_1_0')-1)]
+    
+    # Split data by subject, session, run
+    d$run_id <- paste(d$subject, d$session, d$run, sep = '_')
+    d_split <- split(d, d$run_id)
+    
+    
+    # Test it out
+    run_matrices <- extract_matrices(d_split[[1]], eeg_cols, fmri_cols)
+    cor_mat <- compute_spearman_cor(run_matrices$eeg, run_matrices$fmri)
+    plan(multisession, workers=parallel::detectCores()-5)
+    
+    cors <- future_lapply(d_split, function(run_df) {
+        mats <- extract_matrices(run_df, eeg_cols, fmri_cols)
+        compute_spearman_cor(mats$eeg, mats$fmri)
+    })
+    
+    
+    saveRDS(cors, file = path(root, 'cors.rds'))
+    
+
+format_cors <- function(data, label) {
+    data <- data.frame(t(data))
+    info <- str_split(label, '_')[[1]]
+    subject <- info[1]
+    session <- info[2]
+    run <- info[3]
+    
+    header <- data.frame(subject=subject, session=session, run=run, eeg_feature=rownames(data))
+    out <- cbind(header, data)
+}
+
+
+ch_names <- readRDS(path(root, 'ch_names.rds'))
+
+# Combine
+d <- do.call(rbind, lapply(seq_along(cors), function(i) format_cors(cors[[i]], names(cors[i]))))
+d <- d %>% 
+    separate(eeg_feature, into = c('channel', 'frequency', 'lag'), sep = '_') %>%
+    mutate(frequency = as.numeric(frequency),
+           lag = as.numeric(lag),
+           channel = factor(channel, levels = ch_names))
+
+fmri_cols <- colnames(d)[(which(colnames(d) == 'lag')+1):(ncol(d))]
+
+d <- d %>% 
+    gather(network, cors, all_of(fmri_cols)) %>% 
+    mutate(network = recode(network, `FPCNa` = 'FPNa', `FPCNb` = 'FPNb',
+                            `DAN` = 'dATN', `DANa` = 'dATN-A', `DANb` = 'dATN-B'))
+
+# Save
+write_feather(d, path(root, 'correlations_long.feather'))
+
+
+
+
+
+
+
