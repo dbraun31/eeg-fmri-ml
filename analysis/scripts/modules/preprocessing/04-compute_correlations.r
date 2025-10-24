51bafa08fa59391675fe6fe0d9983dc8bf550a29
Dave <dbraun31@gmail.com>
2025-10-24 12:03:59 -0400

    Update 04-compute_correlations.r

--- analysis/scripts/modules/preprocessing/04-compute_correlations.r
+++ analysis/scripts/modules/preprocessing/04-compute_correlations.r
@@ -7,49 +7,26 @@ library(future.apply)
 library(here)
 library(fs)
 library(arrow)
-source(path(here(), 'analysis/scripts/modules/preprocessing/04a-compile_condition_codes.r'))
+script_root <- path(here(), 'analysis/scripts/modules/preprocessing')
+source(path(script_root, '04a-compile_condition_codes.r'))
 
-# Function defs
-
-extract_matrices <- function(run_df, eeg_cols, fmri_cols) {
-    # Given a data frame for one run
-    # Compute two separate matrices for a given run
-    # One for EEG data, one for fMRI data
-    # fMRI matrix is (TRs, networks)
-    # Returns a list with the EEG and fMRI matrices
-    
-    eeg_mat <- as.matrix(run_df[, eeg_cols])
-    fmri_mat <- as.matrix(run_df[, fmri_cols])
-    return(list(eeg=eeg_mat, fmri=fmri_mat))
-}
-
-compute_spearman_cor <- function(eeg, fmri) {
-    # Given EEG and fMRI matrices
-    # Return an fmri_networks X eeg_features matrix of correlations for each run
-    
-    # Apply column-wise rank
-    eeg_rank <- apply(eeg, 2, rank)
-    fmri_rank <- apply(fmri, 2, rank)
-    
-    # Normalize
-    eeg_z <- scale(eeg_rank)
-    fmri_z <- scale(fmri_rank)
-    n_trs <- nrow(eeg)
-    
-    # Correlate
-    cor <- t(fmri_z) %*% eeg_z / (n_trs - 1)
-    
-    return(cor)
-    
-}
-
+# --- FUNCTION DEFINITIONS --- #
+# ---------------------------- #
 
 # Calculate correlations
 
-calculate_correlations <- function(d, by_task=FALSE) {
+compute_correlations <- function(d, script_root, by_task=FALSE) {
     
-    # Extract modality columns
-    first_eeg_col <- colnames(select(d, starts_with('Fp1')))[1]
+    setDT(d)
+    
+    # Make cache dir
+    if (!dir.exists(path(script_root, 'cache'))) {
+        dir.create(path(script_root, 'cache'))
+    }
+    
+    # Identify EEG and fMRI columns
+    first_eeg_col <- colnames(d)[which(grepl('^Fp1', colnames(d)))[1]]
+    if (is.na(first_eeg_col)) stop('No EEG column starting with "Fp1" found.')
     eeg_cols <- colnames(d)[which(colnames(d) == first_eeg_col):(ncol(d))]
     fmri_cols <- colnames(d)[(which(colnames(d) == 'tr')+1):(which(colnames(d) == first_eeg_col)-1)]
     
@@ -64,29 +41,45 @@ calculate_correlations <- function(d, by_task=FALSE) {
     
     # Split data by subject, session, run
     if (by_task) {
-        d$run_id <- paste(d$subject, d$session, d$run, d$condition, sep = '_')
+        d[, run_id := paste(subject, session, run, condition, sep = '_')]
     } else {
-        d$run_id <- paste(d$subject, d$session, d$run, sep = '_')
+        d[, run_id := paste(subject, session, run, sep = '_')]
     }
     
-    d_split <- split(d, d$run_id)
-    
-    # Calculate correlations in parallel
-    plan(multisession, workers=parallel::detectCores()-5)
+    # Get unique run_ids to distribute to workers
+    run_ids <- unique(d$run_id)
+
+    # Plan parallelization
+    workers <- max(1, parallel::detectCores()-10)
+    if (interactive() && .Platform$OS.type == 'unix') {
+        # Interactive RStudio, use safer multisession
+        plan(multisession, workers = 1)
+    } else if (.Platform$OS.type == 'unix') {
+        plan(multicore, workers=workers)
+    } else {
+        plan(multisession, workers = min(workers, 2))
+    }
+
+    # Increase globals size to avoid serialization errors
+    options(future.globals.maxSize = 8 * 1024^3) # 8 GB
     
     print('Computing correlations...')
-    cors <- future_lapply(d_split, function(run_df) {
-        mats <- extract_matrices(run_df, eeg_cols, fmri_cols)
-        compute_spearman_cor(mats$eeg, mats$fmri)
-    })
-    
-    return(cors)
-    
+
+    future_lapply(run_ids, function(rid) {
+        run_df <- d[run_id == rid]
+        eeg_mat <- as.matrix(run_df[, ..eeg_cols])
+        fmri_mat <- as.matrix(run_df[, ..fmri_cols])
+        cor_mat <- cor(eeg_mat, fmri_mat, method = 'spearman')
+        formatted <- format_cors(cor_mat, rid, by_task = by_task)
+        write_feather(formatted, path(script_root, glue('cache/{rid}.feather')))
+        NULL
+    }, future.seed = FALSE)
+
+    return(run_ids)
+
 }
 
 format_cors <- function(data, label, by_task=FALSE) {
-    # Transpose
-    data <- data.frame(t(data))
     
     # Parse metadata
     info <- str_split(label, '_')[[1]]
@@ -102,7 +95,16 @@ format_cors <- function(data, label, by_task=FALSE) {
     return(out)
 }
 
-# Look for user command line arguments specifying data path
+# --- end function definitions --- #
+# -------------------------------- #
+
+
+# --- START INPUT PARSING / INTERACTIVE SCRIPT --- #
+# ------------------------------------------------ #
+
+
+# --- INPUT PARSING --- # 
+
 args <- commandArgs(trailingOnly=TRUE)
 
 by_task <- any(args %in% c('-t', '--bytask'))
@@ -115,33 +117,31 @@ if (length(data_root) == 0) {
     data_root <- path(args[1])
 }
 
-print(data_root)
-
-# IF YOU ARE RUNNING FROM THE SCRIPT, YOU CAN SET YOUR PATH TO THE DATA HERE
-# and uncomment below
+# --- INTERACTIVE --- # 
+# if you are running interactively (eg, rstudio) and want a custom data path, 
+# set your path to the data by uncommenting below
 # data_root <- 'path/to/data'
 
-# Import data
+# --- IMPORT DATA --- #
 if (!file.exists(path(data_root, '../correlation_data/merged_data.feather'))) {
     stop('Need to run 03-make_flat_data.py before this script.')
 }
 d <- read_feather(path(data_root, '../correlation_data/merged_data.feather'))
 
-
 # Get channel names
 ch_names <- suppressWarnings(readLines(path(data_root, '../correlation_data/ch_names.txt')))
 
 # -- GET CORRELATIONS -- # 
-cors <- calculate_correlations(d, by_task=by_task)
-# Cors is a list where each element has name 'sub-\d\d\d_ses-\d\d\d_run-\d\d\d'
-# and shape (fmri_networks, eeg_features)
+run_ids <- compute_correlations(d, script_root)
+cache_dir <- path(script_root, 'cache')
+rm(d)
+gc()
+
+# Import correlations from cache
+files <- dir_ls(cache_dir, glob = '*.feather')
+d <- do.call(rbind, lapply(files, read_feather))
 
 # -- FORMAT RESULT -- #
-# For each (fmri_networks, eeg_features) matrix, transpose it, add it subject/session/run
-# info, and concatenate
-print('Formatting the result...')
-d <- do.call(rbind, lapply(seq_along(cors), function(i) format_cors(cors[[i]], names(cors[i]), by_task=by_task)))
-
 d <- d %>% 
     separate(eeg_feature, into = c('channel', 'frequency', 'lag'), sep = '_') %>%
     mutate(frequency = as.numeric(frequency),
@@ -154,12 +154,15 @@ fmri_cols <- fmri_cols[fmri_cols != 'task']
 d <- d %>% 
     gather(network, cors, all_of(fmri_cols)) 
 
-# Save
+# --- WRITE TO FILE --- #
 file <- ifelse(by_task, 'correlations_long_bytask.feather', 'correlations_long.feather')
 write_feather(d, path(data_root, glue('../correlation_data/{file}')))
 
 
-
+# delete cache
+dir_delete(cache_dir)
+rm(list = ls())
+gc()
 
 
 
